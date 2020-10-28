547826c69f4e8fbc32bac19a7f1a393876165ec6
ks-test2020 <xy36fkgb3@i.softbank.jp>
2020-10-28 15:29:00 +0900

    modified

--- service-worker.js
+++ service-worker.js
@@ -23,7 +23,7 @@ self.addEventListener('install', function(e) {
         .then(
             function(cache) {
                 // 指定したリソースをキャッシュへ追加
-                return caches.addALl(urlsToCache);
+                return caches.addAll(urlsToCache);
             })
         );
     console.log('[ServiceWorker] Install');
