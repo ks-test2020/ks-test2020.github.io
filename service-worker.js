f1531aceccf42d74bf5ff1c27751933c7259f87f
ks-test2020 <xy36fkgb3@i.softbank.jp>
2020-10-28 20:15:19 +0900

    modified

--- service-worker.js
+++ service-worker.js
@@ -1,3 +1,5 @@
+'use strict';
+
 var CACHE_NAME  = 'test-cache-v8-10';
 var urlsToCache = [
     '/',
@@ -112,3 +114,20 @@ self.addEventListener('fetch', function(event) {
         );
     }
 });
+self.addEventListener('push', function (event) {
+    console.log('Received a push message', event);
+    var title = "プッシュ通知です！";
+    var body = "プッシュ通知はこのようにして送られるのです";
+
+    event.waitUntil(
+        self.registration.showNotification(title, {
+            body: body,
+            icon: 'http://free-images.gatag.net/images/201108090000.jpg',
+            tag: 'push-notification-tag'
+        })
+    );
+});
+self.addEventListener('notificationclick', function (event) {
+    event.notification.close();
+    clients.openWindow("/");
+}, false);
