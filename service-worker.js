f06a04ed854ca99a27d5e08b3bdf91ea8f5b8cb0
ks-test2020 <xy36fkgb3@i.softbank.jp>
2020-10-28 15:22:12 +0900

    modified

--- service-worker.js
+++ service-worker.js
@@ -132,3 +132,18 @@ self.addEventListener('fetch', function(event) {
         );
     }
 });
+self.addEventListener("push", function (event) {
+  if (event.data) {
+     console.log(event.data.text());
+  } else {
+     console.log('No Data');
+  }
+
+  const title ="test";
+  const options = {
+    body: "body",
+    icon: './icons/freeicon-192.png',
+    badge: '',
+  };
+  event.waitUntil(self.registration.showNotification(title, options));
+  });
