3f66ee407357ec550b13f47054facbc23c1d89b1
ks-test2020 <xy36fkgb3@i.softbank.jp>
2020-10-30 18:35:50 +0900

    modified

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -103,6 +103,23 @@ self.addEventListener('fetch', event => {
     );
 });
 
+// フォアグラウンドでのプッシュ通知受信
+messaging.onMessage(function(payload) {
+    var notificationTitle = payload.data.title; // タイトル
+    var notificationOptions = {
+      body: payload.data.body, // 本文
+      icon: '/pwa_512.png', // アイコン
+      click_action: 'https://xxxx.sample.com/' // 飛び先URL
+    };
+ 
+    if (!("Notification" in window)) {
+        // ブラウザが通知機能に対応しているかを判定
+    } else if (Notification.permission === "granted") {
+        // 通知許可されていたら通知する
+        var notification = new Notification(notificationTitle,notificationOptions);
+    }
+});
+
 // バックグラウンドでのプッシュ通知受信
 messaging.setBackgroundMessageHandler(function(payload) {
     console.log('[firebase-messaging-sw.js] Received background message ', payload);
