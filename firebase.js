cdd07c4a35b2b036fba7c2ebae720c8d85e66cf4
ks-test2020 <xy36fkgb3@i.softbank.jp>
2020-10-30 14:52:38 +0900

    modified

--- firebase.js
+++ firebase.js
@@ -13,10 +13,166 @@
   // Initialize Firebase
   firebase.initializeApp(firebaseConfig);
   const messaging = firebase.messaging();
-  messaging.usePublicVapidKey("BCdd5izfiIH5187pj6xbN5Deo-u-jLaahZ3bXqHT_NCiIwbCc5lhvo5i9FufXynJaEixMUoIEu4uZpvB45kfq7I");
+//  messaging.usePublicVapidKey("BCdd5izfiIH5187pj6xbN5Deo-u-jLaahZ3bXqHT_NCiIwbCc5lhvo5i9FufXynJaEixMUoIEu4uZpvB45kfq7I");
 
 var db = firebase.firestore();
 var usersRef = db.collection("users");
+
+// IDs of divs that display registration token UI or request permission UI.
+  const tokenDivId = 'token_div';
+  const permissionDivId = 'permission_div';
+
+  // [START receive_message]
+  // Handle incoming messages. Called when:
+  // - a message is received while the app has focus
+  // - the user clicks on an app notification created by a service worker
+  //   `messaging.setBackgroundMessageHandler` handler.
+  messaging.onMessage((payload) => {
+    console.log('Message received. ', payload);
+    // [START_EXCLUDE]
+    // Update the UI to include the received message.
+    appendMessage(payload);
+    // [END_EXCLUDE]
+  });
+  // [END receive_message]
+
+  function resetUI() {
+    clearMessages();
+    showToken('loading...');
+    // [START get_token]
+    // Get registration token. Initially this makes a network call, once retrieved
+    // subsequent calls to getToken will return from cache.
+    messaging.getToken({vapidKey: 'BCdd5izfiIH5187pj6xbN5Deo-u-jLaahZ3bXqHT_NCiIwbCc5lhvo5i9FufXynJaEixMUoIEu4uZpvB45kfq7I'}).then((currentToken) => {
+      if (currentToken) {
+        sendTokenToServer(currentToken);
+        updateUIForPushEnabled(currentToken);
+      } else {
+        // Show permission request.
+        console.log('No registration token available. Request permission to generate one.');
+        // Show permission UI.
+        updateUIForPushPermissionRequired();
+        setTokenSentToServer(false);
+      }
+    }).catch((err) => {
+      console.log('An error occurred while retrieving token. ', err);
+      showToken('Error retrieving registration token. ', err);
+      setTokenSentToServer(false);
+    });
+    // [END get_token]
+  }
+
+
+  function showToken(currentToken) {
+    // Show token in console and UI.
+    const tokenElement = document.querySelector('#token');
+    tokenElement.textContent = currentToken;
+  }
+
+  // Send the registration token your application server, so that it can:
+  // - send messages back to this app
+  // - subscribe/unsubscribe the token from topics
+  function sendTokenToServer(currentToken) {
+    if (!isTokenSentToServer()) {
+      console.log('Sending token to server...');
+      // TODO(developer): Send the current token to your server.
+      setTokenSentToServer(true);
+    } else {
+      console.log('Token already sent to server so won\'t send it again ' +
+          'unless it changes');
+    }
+
+  }
+
+  function isTokenSentToServer() {
+    return window.localStorage.getItem('sentToServer') === '1';
+  }
+
+  function setTokenSentToServer(sent) {
+    window.localStorage.setItem('sentToServer', sent ? '1' : '0');
+  }
+
+  function showHideDiv(divId, show) {
+    const div = document.querySelector('#' + divId);
+    if (show) {
+      div.style = 'display: visible';
+    } else {
+      div.style = 'display: none';
+    }
+  }
+
+  function requestPermission() {
+    console.log('Requesting permission...');
+    // [START request_permission]
+    Notification.requestPermission().then((permission) => {
+      if (permission === 'granted') {
+        console.log('Notification permission granted.');
+        // TODO(developer): Retrieve a registration token for use with FCM.
+        // [START_EXCLUDE]
+        // In many cases once an app has been granted notification permission,
+        // it should update its UI reflecting this.
+        resetUI();
+        // [END_EXCLUDE]
+      } else {
+        console.log('Unable to get permission to notify.');
+      }
+    });
+    // [END request_permission]
+  }
+
+  function deleteToken() {
+    // Delete registraion token.
+    // [START delete_token]
+    messaging.getToken({vapidKey: 'BCdd5izfiIH5187pj6xbN5Deo-u-jLaahZ3bXqHT_NCiIwbCc5lhvo5i9FufXynJaEixMUoIEu4uZpvB45kfq7I'}).then((currentToken) => {
+      messaging.deleteToken(currentToken).then(() => {
+        console.log('Token deleted.');
+        setTokenSentToServer(false);
+        // [START_EXCLUDE]
+        // Once token is deleted update UI.
+        resetUI();
+        // [END_EXCLUDE]
+      }).catch((err) => {
+        console.log('Unable to delete token. ', err);
+      });
+      // [END delete_token]
+    }).catch((err) => {
+      console.log('Error retrieving registration token. ', err);
+      showToken('Error retrieving registration token. ', err);
+    });
+
+  }
+
+  // Add a message to the messages element.
+  function appendMessage(payload) {
+    const messagesElement = document.querySelector('#messages');
+    const dataHeaderElement = document.createElement('h5');
+    const dataElement = document.createElement('pre');
+    dataElement.style = 'overflow-x:hidden;';
+    dataHeaderElement.textContent = 'Received message:';
+    dataElement.textContent = JSON.stringify(payload, null, 2);
+    messagesElement.appendChild(dataHeaderElement);
+    messagesElement.appendChild(dataElement);
+  }
+
+  // Clear the messages element of all children.
+  function clearMessages() {
+    const messagesElement = document.querySelector('#messages');
+    while (messagesElement.hasChildNodes()) {
+      messagesElement.removeChild(messagesElement.lastChild);
+    }
+  }
+
+  function updateUIForPushEnabled(currentToken) {
+    showHideDiv(tokenDivId, true);
+    showHideDiv(permissionDivId, false);
+    showToken(currentToken);
+  }
+
+  function updateUIForPushPermissionRequired() {
+    showHideDiv(tokenDivId, false);
+    showHideDiv(permissionDivId, true);
+  }
+
+  resetUI();
 
 // 購読確認を行う
 //checkSubscription();
@@ -26,7 +182,7 @@ function checkSubscription() {
     //通知の承認を確認
     messaging.requestPermission().then(function() {
         //トークンを確認
-        messaging.getToken().then(function(token) {
+        messaging.getToken({vapidKey: 'BCdd5izfiIH5187pj6xbN5Deo-u-jLaahZ3bXqHT_NCiIwbCc5lhvo5i9FufXynJaEixMUoIEu4uZpvB45kfq7I'}).then(function(token) {
             //トークン発行
             if (token) {
                 //トークンがDBに入っているか確認
@@ -75,9 +231,9 @@ function checkSubscription() {
 function getSubscription() {
     //通知の承認を確認
   messaging.requestPermission().then(function() {
-console.log(messaging.getToken());
+console.log(messaging.getToken({vapidKey: 'BCdd5izfiIH5187pj6xbN5Deo-u-jLaahZ3bXqHT_NCiIwbCc5lhvo5i9FufXynJaEixMUoIEu4uZpvB45kfq7I'}));
         //トークンを確認
-        messaging.getToken().then(function(token) {
+        messaging.getToken({vapidKey: 'BCdd5izfiIH5187pj6xbN5Deo-u-jLaahZ3bXqHT_NCiIwbCc5lhvo5i9FufXynJaEixMUoIEu4uZpvB45kfq7I'}).then(function(token) {
             //トークン発行
             if (token) {
                 //トークンがDBに入っているか確認
@@ -125,7 +281,7 @@ function removeSubscription() {
     //通知の承認を確認
     messaging.requestPermission().then(function() {
         //トークンを確認
-        messaging.getToken().then(function(token) {
+        messaging.getToken({vapidKey: 'BCdd5izfiIH5187pj6xbN5Deo-u-jLaahZ3bXqHT_NCiIwbCc5lhvo5i9FufXynJaEixMUoIEu4uZpvB45kfq7I'}).then(function(token) {
             //トークン発行
             if (token) {
                 //トークンがDBに入っているか確認
@@ -168,7 +324,7 @@ function removeSubscription() {
  
 //　トークン表示
 function displayToken() {
-  messaging.getToken().then(token => {
+  messaging.getToken({vapidKey: 'BCdd5izfiIH5187pj6xbN5Deo-u-jLaahZ3bXqHT_NCiIwbCc5lhvo5i9FufXynJaEixMUoIEu4uZpvB45kfq7I'}).then(token => {
         if (token) {
             console.log(token);
         } else {
